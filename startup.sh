6c4ed518e657320f0841e73d936ea3d1de57083c
Peter Zrim <peter.zrim@gmail.com>
2024-11-30 22:29:25 +0100

    Updated settings module path and startup script

--- startup.sh
+++ startup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Collect static files
+python manage.py collectstatic --noinput
+
 # Make migrations
 python manage.py makemigrations
 python manage.py migrate
 
-# Start Gunicorn
-gunicorn --bind=0.0.0.0:8000 core.wsgi:application
+# Start Gunicorn with base settings
+DJANGO_SETTINGS_MODULE=core.settings.base gunicorn --bind=0.0.0.0:8000 core.wsgi:application
